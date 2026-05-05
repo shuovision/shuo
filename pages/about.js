d4e7e0a0f2b5d9b1ecac7e887da21b61623b9994
shuovision <shuovision@gmail.com>
2026-05-06 01:32:04 +0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,3 @@
 export default function About() {
-  return <div className="bg-[#0F0F0F] min-h-screen text-white p-20"><h1>About Shuo Vision</h1><a href="/">Back</a></div>
+  return <div className="bg-black min-h-screen text-white p-20"><h1>About Us</h1><a href="/" className="text-[#D4AF37]">Back Home</a></div>
 }
