d7a4dba681882b3f72e1dfb48f09b8566f326aa0
shuovision <shuovision@gmail.com>
2026-05-06 01:34:29 +0800

    Update services.js

--- pages/services.js
+++ pages/services.js
@@ -1,3 +1,3 @@
 export default function Services() {
-  return <div className="bg-black min-h-screen text-white p-20"><h1>Our Services</h1><a href="/" className="text-[#D4AF37]">Back Home</a></div>
+  return <div className="bg-black min-h-screen text-white p-20"><h1>Our Services</h1><br/><a href="/" className="text-[#D4AF37]">← Back Home</a></div>
 }
